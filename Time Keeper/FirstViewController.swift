b570850301b8b510afe85a31f9d858f4e0595609
Ken Driscoll <driscoll.ken@gmail.com>
2014-07-12 15:03:31 -0500

    Initial Commit with Generated Code

---
+++ Time Keeper/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Time Keeper
+//
+//  Created by Ken Driscoll on 7/12/14.
+//  Copyright (c) 2014 Ken Driscoll. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
